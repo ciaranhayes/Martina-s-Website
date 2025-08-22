6f21763ea45df1033b7a848102703205f3724eb0
Ciaran Hayes <hayes.c.hayes@gmail.com>
2025-08-22 18:52:28 +0200

    Coloured boxes component is done

--- martina/app/page.tsx
+++ martina/app/page.tsx
@@ -1,11 +1,13 @@
 import Header from "@/components/header"
 import ContentContainer from "@/components/items-containers"
+import ContentContainerColoured from "@/components/item-contain-coloured"
 
 export default function Page() {
   return (
     <>
       <Header />
       <ContentContainer />
+      <ContentContainerColoured />
     </> 
   )
 }
--- martina/components/item-contain-coloured.tsx
+++ martina/components/item-contain-coloured.tsx
@@ -69,21 +69,21 @@ const posts = [
     }
 ]
 
-export default function ContentContainer() {
+export default function ContentContainerColoured() {
     return (
-        <div className="text-[#292842]">
+        <div className="text-white">
             <div className="mx-auto max-w-7xl px-6 lg:px-8">
                 <div className="mx-auto mt-10 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:mt-16 sm:pt-16 lg:mx-0 lg:max-w-none lg:grid-cols-4 gap-4">
                     {posts.map((post) => (
-                        <article key={post.id} className="flex max-w-xl flex-col items-start justify-between border-l border-[#8A2BE2] p-5">
+                        <article key={post.id} className="flex max-w-xl flex-col items-start justify-between bg-[#8A2BE2] rounded p-10">
                             <div className="group relative grow">
-                                <h3 className="mt-3 text-lg/6 font-semibold text-gray-900 group-hover:text-gray-600">
+                                <h3 className="mt-3 text-lg/6 font-semibold group-hover:text-gray-600">
                                     <Link href={post.href}>
                                         <span className="absolute inset-0" />
                                         {post.title}
                                     </Link>
                                 </h3>
-                                <p className="mt-5 line-clamp-3 text-sm/9">{post.description}</p>
+                                <p className="mt-5 line-clamp-3 text-sm/6">{post.description}</p>
                             </div>
                         </article>
                     ))}
